2b65238d74295440c28d2a2900588ebf57da8323
Nguyễn Thanh Long <longnt@ftech.ai>
2020-04-15 14:28:57 +0700

    Focus on main character

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Com.LuisPedroFonseca.ProCamera2D;
 public class GameController : MonoBehaviour
 {
     public static GameController Instance;
     List<Entity> entities = new List<Entity>();
+    public ProCamera2D cam { private set; get; }
     private void Awake()
     {
+        cam = Camera.main.GetComponent<ProCamera2D>();
         Instance = this;
         CharacterController a = new GameObject("Character").AddComponent<CharacterController>();
+        cam.AddCameraTarget(a.transform);
         a.Setup(5, 5, 0.8f);
     }
 
